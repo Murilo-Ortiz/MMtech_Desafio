bacf53d621ffee992a5b9cbefad7b0aad2eaceb5
Murilo-Ortiz <imurilo@DESKTOP-4KEQF5A.localdomain>
2025-07-04 23:16:47 -0300

    feat: implementado a comunicação do frontend com a api (endpoints)

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -4,6 +4,43 @@ const api = axios.create({
   baseURL: 'http://localhost:3001',
 });
 
-export const getAllData = () => {
-  return api.get(`/data`);
+/**
+ * @param {string} [searchterm='']
+ * @returns {Promise}
+ */
+export const getAllData = (searchterm='') => {
+  return api.get(`/data?search = ${searchterm}`);
+};
+
+/**
+ * @param {string} id
+ * @returns {Promise} 
+**/
+export const getbyId =(id) =>{
+    return api.get(`/data/${id}`, id);
+};
+
+/**
+ * @param {object} newData
+ * @returns {Promise}
+ */
+export const postNewContato = (newData) =>{
+    return api.post('/data', newData);
+};
+
+/**
+ * @param {string} id 
+ * @returns {Promise}
+ */
+export const deleteContato = (id) =>{
+    return api.delete(`/data/${id}`, id);
+};
+
+/**
+ * @param {string} id
+ * @param {object} updatedData
+ * @returns {Promise}
+ */
+export const updateContato = (id, updatedData) =>{
+    return api.put(`/data/${id}`, updatedData)
 };
