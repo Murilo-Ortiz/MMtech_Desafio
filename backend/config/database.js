aab64791ed95cf5b7cd8cc1a166fe202a675cc34
Murilo-Ortiz <imurilo@DESKTOP-4KEQF5A.localdomain>
2025-07-16 19:33:57 -0300

    feat: implementação de login com JWT (incial - registro)

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,4 +1,4 @@
 const Datastore = require('nedb-promises');
 const db = Datastore.create({ filename: './data.db', autoload: true });
-
-module.exports = db;
+const usersDb = Datastore.create({ filename: './users.db', autoload: true });
+module.exports = {db, usersDb};
---
+++ backend/src/authController.js
@@ -0,0 +1,60 @@
+const bycript = require('bycriptsjs');
+const nodemailer = require('nodemailer');
+const crypto = require('crypto');
+const {userDb}=require('../../config/database');
+
+exports.register = async (req, res)=> {
+    try{
+        const{nome, email, password} = req.body;
+
+        if(!nome||!email|| !password){
+            return res.status(400).json({message: 'Todos os campos são obrigatórios'});
+        }
+
+        const existingUser = await userDb.findOne({email});
+        if(existingUser){
+            return res.status.(409).json({message:'Este email já está em uso'});
+        }
+
+        const salt = await bycript.getSalt(10);
+        const hashedPassword = await bycript.hash(password, salt);
+
+        const verificationToken = crypto.randomBytes(32).toString('hex');
+
+        const newUser = {
+            nome,
+            email,
+            password: hashedPassword,
+            isVerified: false,
+            verificationToken,
+        };
+        await userDb.insert(newUser);
+
+        let testAccount = await nodemailer.createTestAccount();
+        let transporter = nodemailer.createTransport({
+            host: 'smtp.ethreal.email',
+            port: 587,
+            secure: false,
+            auth:{
+                user: testAccount.user,
+                pass: testAccount.pass,
+            },
+        });
+
+        const verificationLink = `http://localhost:3001/auth/verify-email?token=${verificationToken}`;
+        let info = await transporter.sendMail({
+            from: '"Agenda MMTech" <noreply@mmtech.com>',
+            to: email,
+            subject: 'Verifique seu e-mail',
+            html: '<p> Olá ${nome}, </p><p>Obrigado por se Registrar! Por favor, clique no link abaixo para verificar sua conta:</p><a href="${verificationLink}">${verificationLink}<a>', 
+        });
+
+        console.log('Email de verificação enviado: \%s', nodemailer.getTestMessageUrl(info));
+
+        res.status(201).json({message:'Usuário Registrado com sucesso! PorFavor, verifique seu e-mail'});
+
+    }catch(error){
+        console.error(error);
+        res.status(500).json({message: 'Erro no servidor ao registrar o usuário'});
+    }
+};
