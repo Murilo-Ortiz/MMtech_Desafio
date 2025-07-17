0f38591701452a04edb924411274244d5599acc1
Murilo-Ortiz <imurilo@DESKTOP-4KEQF5A.localdomain>
2025-07-17 08:23:47 -0300

    feat: proteção de rotas com token e login completos

---
+++ backend/src/autMidlleware.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken');
+
+const protect = (req, res, next) => {
+  let token;
+
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+    try {
+      token = req.headers.authorization.split(' ')[1];
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = decoded.user;
+
+      next();
+    } catch (error) {
+      console.error(error);
+      return res.status(401).json({ message: 'Não autorizado, token falhou.' });
+    }
+  }
+
+  if (!token) {
+    return res.status(401).json({ message: 'Não autorizado, sem token.' });
+  }
+};
+
+module.exports = { protect };
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('./controller');
+const { protect } = require('./autMidlleware');
 
 
-router.get('/', controller.getAllData);
-router.get('/:id', controller.getbyId);
-router.post('/', controller.createContato);
-router.put('/:id', controller.updateContato);
-router.delete('/:id', controller.deleteContato);
+router.get('/', protect, controller.getAllData);
+router.get('/:id', protect, controller.getbyId);
+router.post('/', protect, controller.createContato);
+router.put('/:id', protect, controller.updateContato);
+router.delete('/:id', protect, controller.deleteContato);
 
 module.exports = router;
